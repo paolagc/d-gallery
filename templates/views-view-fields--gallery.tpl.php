7cb8726c9518978bf8f4203a89344bd17ad6bb7d
paolagc <paola.garcia.cadavid@gmail.com>
2015-01-27 09:00:26 -0500

    view template

--- templates/views-view-fields--gallery.tpl.php
+++ templates/views-view-fields--gallery.tpl.php
@@ -1,11 +1,12 @@
 <?php
 	dpm($fields);
-	$thumbnail = !empty($fields['field_image_display']) ? ($fields['field_image_display']->content : (!empty($fields['field_video']) ? $fields['field_video']->content : empty);
+	$thumbnail = $fields['field_image_display']->content ?: $fields['field_video']->content ;
+	dpm($thumbnail);
 ?>
 <?php if(!empty($thumbnail)){
 	print $thumbnail;
 }?>
 <div class="caption">
-	<h3><?php print $title; ?></h3>
+	<h3><?php print $fields['title']->content; ?></h3>
 	<?php print $fields['field_caption']->content; ?>
 </div>
